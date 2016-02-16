669817be0ec04797abf7f902ab7cea059eb54aec
Melchior Rutte <melchior.rutte@gmail.com>
2016-02-16 15:59:22 +0100

    finish dynamic view  migration in Middleman

--- config.rb
+++ config.rb
@@ -56,6 +56,11 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+
+["seb", "anne", "romain", "melchior"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
